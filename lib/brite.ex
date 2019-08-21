726361e17a6f8a36e581930084f0239f8f600e19
Jason Sich <jasich@gmail.com>
2019-08-21 09:35:07 -0400

    Initial commit

---
+++ lib/brite.ex
@@ -0,0 +1,18 @@
+defmodule Brite do
+  @moduledoc """
+  Documentation for Brite.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Brite.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/brite_test.exs
@@ -0,0 +1,8 @@
+defmodule BriteTest do
+  use ExUnit.Case
+  doctest Brite
+
+  test "greets the world" do
+    assert Brite.hello() == :world
+  end
+end
